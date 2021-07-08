ecb500a9b3f5c13aa72035008afdca1ff0a97671
convenies <87023169+convenies@users.noreply.github.com>
2021-07-08 14:43:40 +0400

    Contact Form

--- assets/js/formspree.js
+++ assets/js/formspree.js
@@ -2,7 +2,7 @@ var form = document.getElementById("contactform");
     
 async function handleSubmit(event) {
   event.preventDefault();
-  var status = document.getElementById("my-form-status");
+  var status = document.getElementById("form-status");
   var data = new FormData(event.target);
   fetch(event.target.action, {
     method: form.method,
@@ -11,10 +11,12 @@ async function handleSubmit(event) {
         'Accept': 'application/json'
     }
   }).then(response => {
-    status.innerHTML = "Thank you for getting in touch, we will get back to you soon!";
+    //status.innerHTML = "Thank you for getting in touch, we will get back to you soon!";
+    window.location.href = "#thankyou";
     form.reset()
   }).catch(error => {
-    status.innerHTML = "Oops! There was an error submitting your form. Please try again."
+    //status.innerHTML = "Oops! There was an error submitting the form. Please try again."
+    window.alert("Oops! There was an error submitting the form. Please try again.");
   });
 }
 form.addEventListener("submit", handleSubmit)
